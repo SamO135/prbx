cf23b8a3f632780f81f1bd6442a8292aea1a85a0
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2023-12-14 21:41:40 +0000

    Created all cards in the game

---
+++ prbx_project/all_cards.py
@@ -0,0 +1,444 @@
+from  prbx_project.card import Card
+from  prbx_project.settings import Token
+
+tier1 = [Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 1, Token.GREEN: 1,  Token.BLUE: 1, Token.WHITE: 1, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 1, Token.GREEN: 1,  Token.BLUE: 2, Token.WHITE: 1, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 1, Token.GREEN: 0,  Token.BLUE: 2, Token.WHITE: 2, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 3, Token.GREEN: 1,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 1, Token.GREEN: 2,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 2,  Token.BLUE: 0, Token.WHITE: 2, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=1,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 4, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 1, Token.GREEN: 1,  Token.BLUE: 0, Token.WHITE: 1, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 2, Token.GREEN: 1,  Token.BLUE: 0, Token.WHITE: 1, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 2, Token.GREEN: 2,  Token.BLUE: 0, Token.WHITE: 1, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 1, Token.GREEN: 3,  Token.BLUE: 1, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 1, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 2,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=1,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 4, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.WHITE,
+    price={Token.RED: 1, Token.GREEN: 1,  Token.BLUE: 1, Token.WHITE: 0, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.WHITE,
+    price={Token.RED: 1, Token.GREEN: 2,  Token.BLUE: 1, Token.WHITE: 0, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 2,  Token.BLUE: 2, Token.WHITE: 0, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=1,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 4,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 1, Token.GREEN: 0,  Token.BLUE: 1, Token.WHITE: 1, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 1, Token.GREEN: 0,  Token.BLUE: 1, Token.WHITE: 1, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 2, Token.GREEN: 0,  Token.BLUE: 1, Token.WHITE: 0, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 1,  Token.BLUE: 3, Token.WHITE: 1, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 1, Token.WHITE: 2, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 2, Token.GREEN: 0,  Token.BLUE: 2, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.GREEN,
+    price={Token.RED: 3, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=1,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 4, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 1,  Token.BLUE: 1, Token.WHITE: 1, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 1,  Token.BLUE: 1, Token.WHITE: 2, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 1,  Token.BLUE: 0, Token.WHITE: 2, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 1, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 1, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 1,  Token.BLUE: 2, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 2, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 2, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=0,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+        Card(
+    points=1,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 4, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=1),
+    ]
+
+
+tier2 = [
+        Card(
+    points=1,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 2,  Token.BLUE: 2, Token.WHITE: 3, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.BLACK,
+    price={Token.RED: 2, Token.GREEN: 4,  Token.BLUE: 1, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.BLACK,
+    price={Token.RED: 3, Token.GREEN: 5,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 5, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=3,
+    bonus=Token.BLACK,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 6, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.BLUE,
+    price={Token.RED: 3, Token.GREEN: 2,  Token.BLUE: 2, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 2, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 5, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.BLUE,
+    price={Token.RED: 1, Token.GREEN: 0,  Token.BLUE: 5, Token.WHITE: 2, Token.BLACK: 4, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=3,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 6, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.WHITE,
+    price={Token.RED: 2, Token.GREEN: 3,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.WHITE,
+    price={Token.RED: 3, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 2, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.WHITE,
+    price={Token.RED: 4, Token.GREEN: 1,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.WHITE,
+    price={Token.RED: 5, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.WHITE,
+    price={Token.RED: 5, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=3,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 6, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.GREEN,
+    price={Token.RED: 3, Token.GREEN: 2,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 2, Token.BLACK: 2, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 2, Token.WHITE: 4, Token.BLACK: 1, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 5, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 5,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=3,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 6,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.RED,
+    price={Token.RED: 2, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 2, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=1,
+    bonus=Token.RED,
+    price={Token.RED: 2, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 2,  Token.BLUE: 4, Token.WHITE: 1, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 5, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=2,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 5, Token.YELLOW: 0},
+    tier=2),
+        Card(
+    points=3,
+    bonus=Token.RED,
+    price={Token.RED: 6, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=2),
+]
+
+
+tier3 =  [
+        Card(
+    points=3,
+    bonus=Token.BLACK,
+    price={Token.RED: 3, Token.GREEN: 5,  Token.BLUE: 3, Token.WHITE: 3, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.BLACK,
+    price={Token.RED: 7, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.BLACK,
+    price={Token.RED: 6, Token.GREEN: 3,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=5,
+    bonus=Token.BLACK,
+    price={Token.RED: 7, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=3,
+    bonus=Token.BLUE,
+    price={Token.RED: 3, Token.GREEN: 3,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 5, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 7, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 6, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=5,
+    bonus=Token.BLUE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 7, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=3,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 3, Token.WHITE: 0, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 7, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.WHITE,
+    price={Token.RED: 3, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 6, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=5,
+    bonus=Token.WHITE,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 0, Token.WHITE: 3, Token.BLACK: 7, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=3,
+    bonus=Token.GREEN,
+    price={Token.RED: 3, Token.GREEN: 0,  Token.BLUE: 3, Token.WHITE: 5, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 0,  Token.BLUE: 7, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 6, Token.WHITE: 3, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=5,
+    bonus=Token.GREEN,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 7, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=3,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 3,  Token.BLUE: 5, Token.WHITE: 3, Token.BLACK: 3, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.RED,
+    price={Token.RED: 0, Token.GREEN: 7,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=4,
+    bonus=Token.RED,
+    price={Token.RED: 3, Token.GREEN: 6,  Token.BLUE: 3, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+        Card(
+    points=5,
+    bonus=Token.RED,
+    price={Token.RED: 3, Token.GREEN: 7,  Token.BLUE: 0, Token.WHITE: 0, Token.BLACK: 0, Token.YELLOW: 0},
+    tier=3),
+]
+
+all_cards = [tier1, tier2, tier3]
