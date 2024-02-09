7ac74630f9c4eea41a2b65495e1fd1506b136d6d
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2024-02-09 18:11:27 +0000

    remove commented out code

--- prbx_project/card.py
+++ prbx_project/card.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 from prbx_project.settings import Token
-import random
 
 class Card(BaseModel):
     """A class representing a development card."""
@@ -10,36 +9,3 @@ class Card(BaseModel):
     bonus: Token
     price: dict[Token, int]
     tier: int # Tier 1 is cheapest Tier 3 is most expensive
-
-
-    # Modify this so the generated price matches the card's tier.
-    # def create_price() -> dict[Token, int]:
-    #     """Create a random price for the card.
-        
-    #     Return:
-    #         dict[Token, int]: A dictionary containing the price of the card
-    #     """
-    #     values = [random.randint(0, 3) for i in range(4)]
-    #     tokens = [token for token in Token]
-    #     tokens.remove(Token.YELLOW)
-    #     tokens.remove(random.choice(tokens))
-    #     price = dict(zip(tokens, values))
-    #     return {key: value for key, value in price.items() if value != 0}
-        
-
-    # @classmethod
-    # def new_card(cls, tier=1):
-    #     """A factory method to create new cards.
-        
-    #     Return:
-    #         Card: A new card object
-    #     """
-    #     id = random.randint(1, 1000)
-    #     points = random.randint(0, 4)
-    #     tokens = list(Token)
-    #     tokens.remove(Token.YELLOW)
-    #     bonus = random.choice(tokens)
-    #     # Generate price
-    #     price = Card.create_price()
-    #     return Card(id = id, points=points, bonus=bonus, price=price, tier=1)
-    
